f03806e45fdd8dc277536e99ed7cf78a41655640
niksingh710 <nik.singh710@gmail.com>
2025-02-03 15:49:12 +0530

    nsearch: exposed nsearch package also

--- flake.nix
+++ flake.nix
@@ -10,8 +10,9 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages = {
+        packages = rec {
           default = pkgs.callPackage ./default.nix { };
+          nsearch = default;
 
           nrun = pkgs.writeShellApplication {
             name = "nrun";
